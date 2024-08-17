71cdf81394f75f21136ee8322dbbf22c839607e9
chandro4D <chandroshakhor7@gmail.com>
2024-08-17 10:58:09 +0600

    responsive login page

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,4 +1,3 @@
-
 import { Helmet } from "react-helmet-async";
 import { FcGoogle } from "react-icons/fc";
 import { Link, useLocation, useNavigate } from "react-router-dom";
@@ -8,103 +7,99 @@ import Swal from "sweetalert2";
 
 const Login = () => {
     const { signIn, googleLogin } = useContext(AuthContext);
-    // const axiosPublic = useAxiosPublic();
-
 
     const location = useLocation();
     console.log(location);
     const navigate = useNavigate();
     const from = location.state?.from?.pathname || "/";
 
-
-    const handleLogIn = e => {
+    const handleLogIn = (e) => {
         e.preventDefault();
         const email = e.target.email.value;
         const password = e.target.password.value;
         console.log(email, password);
         signIn(email, password)
-            .then(result => {
+            .then((result) => {
                 console.log(result.user);
                 Swal.fire({
                     icon: "success",
                     text: "LogIn successfully!",
-
                 });
 
                 navigate(from, { replace: true });
             })
-            .catch(error => {
+            .catch((error) => {
                 console.log(error);
                 Swal.fire({
                     icon: "error",
                     text: "Please Provide Correct Email And Password!",
-
                 });
+            });
+    };
 
-            })
-    }
-
-    //--------- Google Login-----------
-    const handleGoogleLogin = e => {
+    const handleGoogleLogin = (e) => {
         e.preventDefault();
         googleLogin()
-        .then(result =>{
-            console.log(result.user)
-            Swal.fire({
-                icon: "success",
-                text: "LogIn successfully!",
-                
-              });
-            navigate(location?.state ? location.state : '/');
-        })
-        .catch(error =>{
-            console.log(error.message);
-            alert(error.message)
-        })
-        
+            .then((result) => {
+                console.log(result.user);
+                Swal.fire({
+                    icon: "success",
+                    text: "LogIn successfully!",
+                });
+                navigate(location?.state ? location.state : "/");
+            })
+            .catch((error) => {
+                console.log(error.message);
+                alert(error.message);
+            });
+    };
 
-    }
     return (
-        <div className="pt-10">
-            <div className=" lg:w-[500px] sm:w-[350px] lg:h-[600px] sm:h-[500px] bg-lime-100  lg:ml-[500px]  mb-10 rounded-xl sm:ml-[0px]">
+        <div className="pt-10 flex justify-center items-center">
+            <div className="w-full max-w-lg lg:w-[500px] lg:h-[600px] sm:w-[350px] sm:h-[500px] bg-lime-100 mx-5 sm:mx-auto mb-10 rounded-xl">
                 <Helmet>
                     <title>OurStore | Login</title>
                 </Helmet>
 
-                <div className=" pt-12">
+                <div className="pt-12">
                     <h2 className="text-center text-2xl font-bold text-pink-400 mb-2">WELCOME TO OUR STORE</h2>
-                    <p className="text-center text-xl font-semibold text-black">Login To Your Account By Entering<br /> Your Email and Password</p>
+                    <p className="text-center text-xl font-semibold text-black">
+                        Login To Your Account By Entering
+                        <br /> Your Email and Password
+                    </p>
                 </div>
-                <form onSubmit={handleLogIn} className="pt-10 lg:pl-12 sm:pl-0">
-                    <div className=" lg:w-[400px] sm:w-[250px] h-[50px]">
+                <form onSubmit={handleLogIn} className="pt-10 px-5 sm:px-12">
+                    <div className="w-full h-[50px]">
                         <input className="w-full h-full rounded-lg text-center" type="email" placeholder="Your Email" required name="email" />
                     </div>
                     <br />
-                    <div className=" lg:w-[400px] sm:w-[250px] h-[50px] ">
+                    <div className="w-full h-[50px]">
                         <input className="w-full h-full text-center rounded-lg" type="password" placeholder="Enter Your Password" required name="password" />
                     </div>
-
                     <br />
-
-                    <div className=" lg:w-[400px] sm:w-[250px]  h-[50px] rounded-2xl">
-                        <button className=" btn btn-outline btn-secondary w-full h-full text-white"> LOGIN</button>
+                    <div className="w-full h-[50px] rounded-2xl">
+                        <button className="btn btn-outline btn-secondary w-full h-full text-white">LOGIN</button>
                     </div>
                 </form>
                 <br />
                 <div data-aos="fade-down" data-aos-easing="linear" data-aos-duration="1450">
-                    <h3 className="text-center text-2xl font-semibold text-yellow-600">Don`t Have An Account? <Link to={"/register"}><span className="text-lime-300" >Register</span></Link></h3>
+                    <h3 className="text-center text-2xl font-semibold text-yellow-600">
+                        Don’t Have An Account?{" "}
+                        <Link to={"/register"}>
+                            <span className="text-lime-300">Register</span>
+                        </Link>
+                    </h3>
                 </div>
                 <br />
                 <div data-aos="fade-down" data-aos-easing="linear" data-aos-duration="1500">
-                    <div className="divider divider-neutral text-xl font-medium ml-14 mr-14">
+                    <div className="divider divider-neutral text-xl font-medium mx-10 sm:mx-14">
                         Continue With
                     </div>
 
-                    <div className="flex  ml-[240px]   mt-6">
-                        <div  >
-                            <button onClick={handleGoogleLogin} className=" mr-8    text-center pt-1 "><FcGoogle className="w-14 h-14"></FcGoogle></button>
-                        </div>
-
+                    <div className="flex justify-center mt-6">
+                        <button onClick={handleGoogleLogin} className=" pt-1">
+                            <FcGoogle className="w-14 h-14" />
+                        </button>
                     </div>
                 </div>
             </div>
@@ -113,3 +108,122 @@ const Login = () => {
 };
 
 export default Login;
+
+
+
+
+
+// import { Helmet } from "react-helmet-async";
+// import { FcGoogle } from "react-icons/fc";
+// import { Link, useLocation, useNavigate } from "react-router-dom";
+// import { AuthContext } from "../../Provider/AuthProvider";
+// import { useContext } from "react";
+// import Swal from "sweetalert2";
+
+// const Login = () => {
+//     const { signIn, googleLogin } = useContext(AuthContext);
+//     // const axiosPublic = useAxiosPublic();
+
+
+//     const location = useLocation();
+//     console.log(location);
+//     const navigate = useNavigate();
+//     const from = location.state?.from?.pathname || "/";
+
+
+//     const handleLogIn = e => {
+//         e.preventDefault();
+//         const email = e.target.email.value;
+//         const password = e.target.password.value;
+//         console.log(email, password);
+//         signIn(email, password)
+//             .then(result => {
+//                 console.log(result.user);
+//                 Swal.fire({
+//                     icon: "success",
+//                     text: "LogIn successfully!",
+
+//                 });
+
+//                 navigate(from, { replace: true });
+//             })
+//             .catch(error => {
+//                 console.log(error);
+//                 Swal.fire({
+//                     icon: "error",
+//                     text: "Please Provide Correct Email And Password!",
+
+//                 });
+
+//             })
+//     }
+
+//     //--------- Google Login-----------
+//     const handleGoogleLogin = e => {
+//         e.preventDefault();
+//         googleLogin()
+//         .then(result =>{
+//             console.log(result.user)
+//             Swal.fire({
+//                 icon: "success",
+//                 text: "LogIn successfully!",
+                
+//               });
+//             navigate(location?.state ? location.state : '/');
+//         })
+//         .catch(error =>{
+//             console.log(error.message);
+//             alert(error.message)
+//         })
+        
+
+//     }
+//     return (
+//         <div className="pt-10">
+//             <div className=" lg:w-[500px] sm:w-[350px] lg:h-[600px] sm:h-[500px] bg-lime-100  lg:ml-[500px]  mb-10 rounded-xl sm:ml-[0px]">
+//                 <Helmet>
+//                     <title>OurStore | Login</title>
+//                 </Helmet>
+
+//                 <div className=" pt-12">
+//                     <h2 className="text-center text-2xl font-bold text-pink-400 mb-2">WELCOME TO OUR STORE</h2>
+//                     <p className="text-center text-xl font-semibold text-black">Login To Your Account By Entering<br /> Your Email and Password</p>
+//                 </div>
+//                 <form onSubmit={handleLogIn} className="pt-10 lg:pl-12 sm:pl-0">
+//                     <div className=" lg:w-[400px] sm:w-[250px] h-[50px]">
+//                         <input className="w-full h-full rounded-lg text-center" type="email" placeholder="Your Email" required name="email" />
+//                     </div>
+//                     <br />
+//                     <div className=" lg:w-[400px] sm:w-[250px] h-[50px] ">
+//                         <input className="w-full h-full text-center rounded-lg" type="password" placeholder="Enter Your Password" required name="password" />
+//                     </div>
+
+//                     <br />
+
+//                     <div className=" lg:w-[400px] sm:w-[250px]  h-[50px] rounded-2xl">
+//                         <button className=" btn btn-outline btn-secondary w-full h-full text-white"> LOGIN</button>
+//                     </div>
+//                 </form>
+//                 <br />
+//                 <div data-aos="fade-down" data-aos-easing="linear" data-aos-duration="1450">
+//                     <h3 className="text-center text-2xl font-semibold text-yellow-600">Don`t Have An Account? <Link to={"/register"}><span className="text-lime-300" >Register</span></Link></h3>
+//                 </div>
+//                 <br />
+//                 <div data-aos="fade-down" data-aos-easing="linear" data-aos-duration="1500">
+//                     <div className="divider divider-neutral text-xl font-medium ml-14 mr-14">
+//                         Continue With
+//                     </div>
+
+//                     <div className="flex  ml-[240px]   mt-6">
+//                         <div  >
+//                             <button onClick={handleGoogleLogin} className=" mr-8    text-center pt-1 "><FcGoogle className="w-14 h-14"></FcGoogle></button>
+//                         </div>
+
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// };
+
+// export default Login;
--- src/Pages/Navbar/Navbar.jsx
+++ src/Pages/Navbar/Navbar.jsx
@@ -1,4 +1,3 @@
-
 import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../Provider/AuthProvider";
@@ -7,56 +6,150 @@ import Swal from "sweetalert2";
 const Navbar = () => {
     const { user, logOut } = useContext(AuthContext);
 
-
     const handleSignOut = () => {
         logOut()
-            .then(result => {
-
+            .then((result) => {
                 Swal.fire({
                     icon: "success",
                     text: "LogOut successfully!",
-
                 });
                 console.log(result.user);
             })
-            .catch()
-    }
+            .catch();
+    };
 
-    const Links = <>
-        <li className="text-xl mt-3  "><Link to="/">Home</Link></li>
+    const Links = (
+        <>
+            <li className="text-xl mt-3 text-gray-950">
+                <Link to="/">Home</Link>
+            </li>
+            {user ? (
+                <li className="text-xl mt-3 text-gray-950">
+                    <button onClick={handleSignOut}>LogOut</button>
+                </li>
+            ) : (
+                <li className="text-xl mt-3 text-gray-950">
+                    <Link to="/login">LogIn</Link>
+                </li>
+            )}
+            <li className="mt-[20xl]">
+                <Link to="/register">
+                    <a className="btn btn-secondary text-xl text-white">Join Us</a>
+                </Link>
+            </li>
+        </>
+    );
 
-        
-        {
-            user? <><button onClick={handleSignOut} className="text-xl mt-3"><Link to="/register">LogOut</Link></button></> : <><li className="text-xl mt-3"><Link to="/login">LogIn</Link></li></>
-        }
-        <Link to="/register"><li ><a className="btn btn-secondary mt-3 text-xl text-white ">Join Us</a></li></Link>
-
-    </>
     return (
-        <div className="navbar  z-10 bg-sky-500 w-full text-white lg:h-32  ">
+        <div className="navbar z-10 bg-sky-500 w-full text-white lg:h-32">
             <div className="navbar-start">
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="h-5 w-5"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M4 6h16M4 12h8m-8 6h16"
+                            />
+                        </svg>
                     </div>
-                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                    <ul
+                        tabIndex={0}
+                        className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+                    >
                         {Links}
                     </ul>
                 </div>
-                <div className="">
-                    <a className="text-white text-4xl font-medium ml-[100px]  pt-3"> <span className="text-purple-600">O</span>ur<span className="text-purple-600">S</span>tore</a>
+                <div className="ml-4 lg:ml-[100px] pt-3">
+                    <a className="text-white text-3xl lg:text-4xl font-medium">
+                        <span className="text-purple-600">O</span>ur
+                        <span className="text-purple-600">S</span>tore
+                    </a>
                 </div>
             </div>
-            <div className=" hidden lg:flex ml-[300px] ">
-                <ul className="menu menu-horizontal ">
-                    {Links}
-                </ul>
+            <div className="hidden lg:flex lg:ml-[300px]">
+                <ul className="menu menu-horizontal space-x-4">{Links}</ul>
             </div>
-
-
-
         </div>
     );
 };
 
 export default Navbar;
+
+
+
+
+
+
+
+
+
+
+// import { useContext } from "react";
+// import { Link } from "react-router-dom";
+// import { AuthContext } from "../../Provider/AuthProvider";
+// import Swal from "sweetalert2";
+
+// const Navbar = () => {
+//     const { user, logOut } = useContext(AuthContext);
+
+
+//     const handleSignOut = () => {
+//         logOut()
+//             .then(result => {
+
+//                 Swal.fire({
+//                     icon: "success",
+//                     text: "LogOut successfully!",
+
+//                 });
+//                 console.log(result.user);
+//             })
+//             .catch()
+//     }
+
+//     const Links = <>
+//         <li className="text-xl mt-3  "><Link to="/">Home</Link></li>
+
+        
+//         {
+//             user? <><button onClick={handleSignOut} className="text-xl mt-3"><Link to="/register">LogOut</Link></button></> : <><li className="text-xl mt-3"><Link to="/login">LogIn</Link></li></>
+//         }
+//         <Link to="/register"><li ><a className="btn btn-secondary mt-3 text-xl text-white ">Join Us</a></li></Link>
+
+//     </>
+//     return (
+//         <div className="navbar  z-10 bg-sky-500 w-full text-white lg:h-32  ">
+//             <div className="navbar-start">
+//                 <div className="dropdown">
+//                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+//                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+//                     </div>
+//                     <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+//                         {Links}
+//                     </ul>
+//                 </div>
+//                 <div className="">
+//                     <a className="text-white text-4xl font-medium ml-[100px]  pt-3"> <span className="text-purple-600">O</span>ur<span className="text-purple-600">S</span>tore</a>
+//                 </div>
+//             </div>
+//             <div className=" hidden lg:flex ml-[300px] ">
+//                 <ul className="menu menu-horizontal ">
+//                     {Links}
+//                 </ul>
+//             </div>
+
+
+
+//         </div>
+//     );
+// };
+
+// export default Navbar;
